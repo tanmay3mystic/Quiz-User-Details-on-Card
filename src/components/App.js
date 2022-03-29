bca347c8fbac7b9416e92d59143dcf8406ccb6c1
Tanmay Rathod <tanmay.gopalrathod@mtap.in>
2022-03-29 10:20:51 +0530

    initial set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./../styles/App.css";
+import Users from './Users'
 
 function App() 
 {
 	return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+	<Users />
 	</div>
 	);
 }
---
+++ src/components/Users.js
@@ -0,0 +1,9 @@
+import react from 'react'
+import {Box} from '@mui/material'
+
+
+export const Users =  ()=>{
+    return <Box>
+        Hi There!!!
+    </Box>
+}
